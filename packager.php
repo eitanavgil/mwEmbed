b2d032c3c9ea9034c828ef764c2a3b2b12d7668b
Michael Dale <michael.dale@kaltura.com>
2010-06-01 01:41:18 +0000

    * package updates * core updates per sync with mwEmbedStandAlone * code cleanup minor bug fixes
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/packager@601 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- packager.php
+++ packager.php
@@ -9,6 +9,21 @@ if( !isset( $_GET['name'] ) ){
 	die( 'Please supply a package name' );
 }
 
+$versionString = '1.1x';
+
+$licenseHeader = '/**
+ * @license
+ * mwEmbed
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * 
+ * @copyright (C) 2010 Kaltura 
+ * @author Michael Dale ( michael.dale at kaltura.com )
+ * 
+ * @url http://www.kaltura.org/project/HTML5_Video_Media_JavaScript_Library
+ *
+ * Libraries used carry code license in headers
+ */';
+
 $packageName = $_GET['name'];
 
 // This class list is used to create a static files in the html5 player package
@@ -43,7 +58,9 @@ $html5PlayerClassList = array(
 	'j.fn.menu',
 );
 $html5PlayerStyleList = array( 
-	// Seperate all the style sheets for easy "editing" 
+	// Separate all the style sheets for path preservation 
+	// ( normally the script-loader serves absolute or relative paths
+	//  based on the scriptLoader location ) 
 	'mw.style.jqueryUiRedmond',
 	'mw.style.mwCommon',
 	'mw.style.EmbedPlayer',
@@ -55,7 +72,7 @@ $html5PlayerStyleList = array(
 
 // Switch among requested packages:
 switch( $packageName ) {
-	case 'kaltura-html5player-dreamweaver-widget':
+	case 'kaltura-html5player-widget':
 		pakageClassList( $packageName,  $html5PlayerClassList,  $html5PlayerStyleList );
 	break;
 	default:
@@ -65,12 +82,15 @@ switch( $packageName ) {
 
 // Get the entire string from the deployed version:
 function pakageClassList($packageName, $html5PlayerClassList, $html5PlayerStyleList ){
+	global $versionString, $licenseHeader;
+	
 	$zip = new ZipArchive();
-	$filename = './' . $packageName . '.zip';
-
-	$rootFileFolder = '/kaltura-html5-player';
-
-	// Remove the old zip if present
+	$filename = './' . $packageName . '.' . $versionString . '.zip';
+	
+	
+	$rootFileFolder = 'kaltura-html5player-widget';	
+	
+	// 	Remove the old zip if present
 	if( is_file( $filename ) ){
 		if( !unlink( $filename ) ){
 			exit( "error could not remove old package: " . $packageName . '.zip');
@@ -147,9 +167,16 @@ function pakageClassList($packageName, $html5PlayerClassList, $html5PlayerStyleL
 	}
 
 	$scriptOutput = ob_get_clean();
+	
+	// Add the license header: 
+	$scriptOutput = $licenseHeader . "\n" . $scriptOutput;
+	
 	// Register the css classes ( static builds inlcude the css explicity )
 	$scriptOutput.= "\n" . implode('=1;', $html5PlayerStyleList) . "=1;\n";
 
+	// Disable local usage of cortado ( GPL package in MIT set) 
+	$scriptOutput.= "\n mw.setConfig( 'relativeCortadoAppletPath', false );\n";
+	
 	// Output the static package to zip file
 	//file_put_contents( '../mwEmbed/mwEmbed-player-static.js', $scriptOutput );
 	$zip->addFromString( $rootFileFolder. "/mwEmbed-player-static.js", $scriptOutput);
@@ -160,8 +187,10 @@ function pakageClassList($packageName, $html5PlayerClassList, $html5PlayerStyleL
 		$objects = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $packageTemplateDir ), RecursiveIteratorIterator::SELF_FIRST );
 		foreach( $objects as $path => $object ) {
 			if( strpos( $path, '.svn' ) == 0 ) {
-				$targetZipPath = str_replace( $packageTemplateDir, '', $path );				
-				$zip->addFile( $path, $rootFileFolder . $targetZipPath );
+				if( is_file( $path )){
+					$targetZipPath = str_replace( $packageTemplateDir, '', $path );							
+					$zip->addFile( $path, $rootFileFolder . $targetZipPath );
+				}
 			}
 		}
 	} else {
@@ -174,7 +203,7 @@ function pakageClassList($packageName, $html5PlayerClassList, $html5PlayerStyleL
 	// Loop over the directories if we find an "image", "swf", or "jar" add it with full path
 	$objects = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( '../mwEmbed/' ), RecursiveIteratorIterator::SELF_FIRST );
 	foreach( $objects as $path => $object ){
-		$ext = substr( $path, -4 );
+		$ext = substr($path, strrpos( $path, '.' )+1 );		
 		$isValidModule = false;
 		foreach( $wgExtensionJavascriptModules as $moduleName => $modulePath ){
 			if( strpos( $path, $modulePath ) !== false ){
@@ -186,15 +215,39 @@ function pakageClassList($packageName, $html5PlayerClassList, $html5PlayerStyleL
 				&& strpos( $path, 'skins/common/'  ) === false ) {
 			//Skip the module not in module list nor is it a jquery asset
 			continue;
-		}
-		
-
-		if( $ext == '.swf' || $ext == '.jar' || $ext == '.gif' || $ext == 'jpeg' || $ext == '.jpg' || $ext == '.png' ){
-			$targetZipPath = str_replace( '../mwEmbed', '', $path);
+		}		
+		if( $ext == 'swf' || $ext == 'js' || $ext == 'gif' || 
+			$ext == 'jpeg' || $ext == 'jpg' || $ext == 'png' || $ext == 'txt'){
+			$targetZipPath = str_replace( '../mwEmbed', '', $path);		
 			$zip->addFile( $path, $rootFileFolder . $targetZipPath );
-		}	
+		}
 	}
 	$zip->close(); 
+	
+	// unzip the file
+	$zip->open(  dirname( __FILE__ ) . "/" . $filename );
+	
+	chdir( dirname( __FILE__ ) .  "/" );
+	// remove the host zip file
+	unlink( $filename );
+	$zipDir = 'temp' . time() ;
+	// PHP zip SUCKS! (windows 7 can't read the zip file ) unzip and zip via command line :(	
+	@$zip->extractTo(  dirname( __FILE__ ) .  "/" .  $zipDir );
+	$zip->close(); 
+	
+	chdir( dirname( __FILE__ ) .  "/" . $zipDir );	
+	// Run a shell compress ( also has weird syntax ) 
+	$zipCmd = "zip -r $packageName $packageName";		
+	exec( $zipCmd );
+	// Go back to package directory
+	chdir( dirname( __FILE__ ) );
+	// move the zip file: 
+	rename( $zipDir .'/'. $packageName . '.zip', $filename );
+	// remove the temporary directory
+	recursiveDelete( $zipDir );
+	
+
+	
 	$zip->open(  dirname( __FILE__ ) . "/" . $filename );
 	// Overide the script-loder header
 	header("Content-Type: text/html");
@@ -212,11 +265,10 @@ function pakageClassList($packageName, $html5PlayerClassList, $html5PlayerStyleL
 					. ' :: ' . $zip->getNameIndex($i) . " \n";
 			}
 			$stats.= "</pre>";
-		
 
-			echo 'Download <a href="'. $packageName .'.zip">' . $packageName. '.zip</a> ( ' . 
-				formatBytes( filesize( realpath( dirname( __FILE__ ) ) . "/{$packageName}.zip")  ). ' )<br />';
-			echo $stats;
+		echo 'Download <a href="'. $packageName . '.' . $versionString .'.zip">' . $packageName. '.' . $versionString . '.zip</a> ( ' . 
+			formatBytes( filesize( dirname( __FILE__ ) . "/" . $filename  ) ). ' )<br />';
+		echo $stats;
 	} else { 
 		echo "Error: " . $zip->getStatusString(); 
 	}
@@ -232,4 +284,17 @@ function formatBytes($bytes, $precision = 2) {
   
     return round($bytes, $precision) . ' ' . $units[$pow];
 } 
+function recursiveDelete( $str ){
+	if(is_file($str)){
+		return @unlink($str);
+	}
+	else if( is_dir( $str ) ) {
+		$scan = glob(rtrim($str,'/').'/*');
+		foreach($scan as $index=>$path){
+			recursiveDelete($path);
+		}
+		return @rmdir($str);
+	}
+}
 ?>
+	

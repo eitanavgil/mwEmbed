a653bf0a23289a603b069d718326bd9481f57c20
Andrew Davis <andrew.davis@kaltura.com>
2010-09-19 21:53:41 +0000

    moved from ksu integration kit to full ksu
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/kplayer-examples@982 b58a29cf-3064-46da-94c6-1c29cc75c8e5

---
+++ mediaRSS-Gallery/ksu.js
@@ -0,0 +1,220 @@
+<!--
+// -----------------------------------------------------------------------------
+// Globals
+// Major version of Flash required
+//var requiredMajorVersion = ${version_major};
+// Minor version of Flash required
+//var requiredMinorVersion = ${version_minor};
+// Minor version of Flash required
+//var requiredRevision = ${version_revision};
+// -----------------------------------------------------------------------------
+// -->
+
+	var app;
+	var delegate = {};
+
+	//KUploader callbacks
+	delegate.readyHandler = function()
+	{
+		app = document.getElementById("uploader");
+    uploaderIsReady();
+	}
+
+	delegate.selectHandler = function()
+	{
+		alert("selectHandler()");
+		console.log(app.getTotalSize());
+    upload();
+	}
+
+	delegate.singleUploadCompleteHandler = function(args)
+	{
+		console.log("singleUploadCompleteHandler", args[0].title);
+	}
+
+	delegate.allUploadsCompleteHandler = function()
+	{
+		alert("allUploadsCompleteHandler");
+	}
+
+	delegate.entriesAddedHandler = function(entries)
+	{
+		alert(entries[0].entryId)
+	}
+
+	delegate.progressHandler = function(args)
+	{
+		console.log(args[2].title + ": " + args[0] + " / " + args[1]);
+	}
+
+	delegate.uiConfErrorHandler = function()
+	{
+		alert("ui conf loading error");
+	}
+
+	//KUplaoder API calls
+
+	function upload()
+	{
+		app.upload();
+	}
+
+	function setTags(tags, startIndex, endIndex)
+	{
+		app.setTags(tags, startIndex, endIndex);
+	}
+
+	function addTags(tags, startIndex, endIndex)
+	{
+		app.addTags(tags, startIndex, endIndex);
+	}
+	function setTitle(title, startIndex, endIndex)
+	{
+		app.setTitle(title, startIndex, endIndex);
+	}
+
+	function getFiles()
+	{
+		var files = app.getFiles();
+		console.log(files[0].title);
+	}
+
+	function addEntries()
+	{
+		app.addEntries();
+	}
+
+	function stopUploads()
+	{
+		app.stopUploads();
+	}
+
+	function setMaxUploads(value)
+	{
+		app.setMaxUploads(value);
+	}
+
+	function browse()
+	{
+		app.browse();
+	}
+
+	function setPartnerData(value)
+	{
+		app.setPartnerData(value);
+	}
+
+	//End of KUploder API calls
+	//--------------------------------------------------------------------
+
+	function setMediaType(mediaType)
+	{
+//		var mediaType = mediaTypeInput.value;
+		app.setMediaType(mediaType);
+	}
+
+	function addTagsFromForm()
+	{
+		var tags = document.getElementById("tagsInput").value.split(",");
+		var startIndex = parseInt(tagsStartIndex.value);
+		var endIndex = parseInt(tagsEndIndex.value);
+		addTags(tags, startIndex, endIndex);
+	}
+
+	function setTagsFromForm()
+	{
+		var tags = document.getElementById("tagsInput").value.split(",");
+		var startIndex = parseInt(tagsStartIndex.value);
+		var endIndex = parseInt(tagsEndIndex.value);
+		setTags(tags, startIndex, endIndex);
+	}
+
+	function setTitleFromForm()
+	{
+		var startIndex = parseInt(titleStartIndex.value);
+		var endIndex = parseInt(titleEndIndex.value);
+		setTitle(titleInput.value, startIndex, endIndex);
+	}
+
+	function removeFilesFromForm()
+	{
+		var startIndex = parseInt(removeStartIndex.value);
+		var endIndex = parseInt(removeEndIndex.value);
+		app.removeFiles(startIndex, endIndex)
+		console.log(app.getTotalSize());
+	}
+
+	function setGroupId(value)
+	{
+		app.setGroupId(value);
+	}
+
+	function setPermissions(value)
+	{
+		app.setPermissions(value);
+	}
+
+	function setSiteUrl(value)
+	{
+		app.setSiteUrl(value);
+	}
+
+	function setScreenName(value)
+	{
+		app.setScreenName(value);
+	}
+
+
+
+	var tagsInput;
+	var tagsStartIndex;
+	var tagsEndIndex;
+
+	var titleInput;
+	var titleStartIndex;
+	var titleEndIndex;
+
+	var removeStartIndex;
+	var removeEndIndex;
+	var maxUploadsInput;
+
+	var partnerDataInput;
+
+	var groupId;
+	var permissions;
+	var screenName;
+	var siteUrl;
+
+	function onLoadHandler()
+	{
+		tagsInput = document.getElementById("tagsInput");
+		tagsStartIndex = document.getElementById("tagsStartIndex");
+		tagsEndIndex = document.getElementById("tagsEndIndex");
+
+		titleInput = document.getElementById("titleInput");
+		titleStartIndex = document.getElementById("titleStartIndex");
+		titleEndIndex = document.getElementById("titleEndIndex");
+
+		removeStartIndex = document.getElementById("removeStartIndex");;
+		removeEndIndex = document.getElementById("removeEndIndex");
+
+		maxUploadsInput = document.getElementById("maxUploadsInput");
+		partnerDataInput = document.getElementById("partnerDataInput");
+
+		groupId = document.getElementById("groupId");
+		permissions = document.getElementById("permissions");
+		screenName = document.getElementById("screenName");
+		siteUrl = document.getElementById("siteUrl");
+	}
+
+
+function uploaderIsReady() {
+  console.log(app);
+	setMediaType("video");
+  }
+
+function saveEntry() {
+  addEntries();
+  setTitleFromForm();
+  setTagsFromForm();
+  }
--- mediaRSS-Gallery/ksu.php
+++ mediaRSS-Gallery/ksu.php
@@ -9,7 +9,7 @@
   include "./ksu-settings.php";
 
 	//define session variables
-	$partnerUserID          = 'ANONYMOUS';
+	$partnerUserID          = 'openvideoconference.tv';
 
 	//Construction of Kaltura objects for session initiation
 	$config           = new KalturaConfiguration(KALTURA_PARTNER_ID);
@@ -23,14 +23,19 @@
 	$flashVars["entryId"] 	 = -1;	     
 	$flashVars["ks"]   = $ks; 
 	$flashVars["conversionProfile"]   = 5; 
-	$flashVars["maxFileSize"]   = 200; 
+	$flashVars["maxFileSize"]   = 250; 
 	$flashVars["maxTotalSize"]   = 5000; 
 	$flashVars["uiConfId"]   = 11500; 
 	$flashVars["jsDelegate"]   = "delegate"; 
 ?>
 
+<link rel="stylesheet" type="text/css" href="history/history.css" />
+<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
-
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<script src="AC_OETags.js" language="javascript"></script>
+<script src="history/history.js" language="javascript"></script>
 <!---set style to enable widget overlap -->
 <style>
 	body { margin: 0px; overflow:hidden }
@@ -40,202 +45,16 @@
 </style>
 
 <!---	JavaScript handler methods to react to upload events. -->
-<script type="text/javascript">
-	var flashObj;
-	var delegate = {};
+<script type="text/javascript" src="./ksu.js">
 
-	//KSU handlers
-	delegate.readyHandler = function()
-	{
-		flashObj = document.getElementById("uploader");
-	}
-
-	delegate.selectHandler = function()
-	{
-		console.log("selectHandler()");
-		console.log(flashObj.getTotalSize());
-	}
-
-	delegate.singleUploadCompleteHandler = function(args)
-	{
-		console.log("singleUploadCompleteHandler", args[0].title);
-	}
-
-	delegate.allUploadsCompleteHandler = function()
-	{
-		console.log("allUploadsCompleteHandler");
-	}
-
-	delegate.entriesAddedHandler = function(entries)
-	{
-		console.log(entries);
-	}
-
-	delegate.progressHandler = function(args)
-	{
-		console.log(args[2].title + ": " + args[0] + " / " + args[1]);
-	}
-
-	delegate.uiConfErrorHandler = function()
-	{
-		console.log("ui conf loading error");
-	}
-
-	<!--- JavaScript callback methods to activate Kaltura services via the KSU widget.-->
-	function upload()
-	{
-		flashObj.upload();
-	}
-
-	function setTags(tags, startIndex, endIndex)
-	{
-		flashObj.setTags(tags, startIndex, endIndex);
-	}
-
-	function addTags(tags, startIndex, endIndex)
-	{
-		flashObj.addTags(tags, startIndex, endIndex);
-	}
-	function setTitle(title, startIndex, endIndex)
-	{
-		flashObj.setTitle(title, startIndex, endIndex);
-	}
-
-	function getFiles()
-	{
-		var files = flashObj.getFiles();
-		console.log(files[0].title);
-	}
-
-	function addEntries()
-	{
-		flashObj.addEntries();
-	}
-
-	function stopUploads()
-	{
-		flashObj.stopUploads();
-	}
-
-	function setMaxUploads(value)
-	{
-		flashObj.setMaxUploads(value);
-	}
-	
-		function setPartnerData(value)
-	{
-		flashObj.setPartnerData(value);
-	}
-
-	
-	function setMediaType()
-	{
-		var mediaType = mediaTypeInput.value;
-		console.log(mediaType);
-		flashObj.setMediaType(mediaType);
-	}
-
-	function addTagsFromForm()
-	{
-		var tags = document.getElementById("tagsInput").value.split(",");
-		var startIndex = parseInt(tagsStartIndex.value);
-		var endIndex = parseInt(tagsEndIndex.value);
-		addTags(tags, startIndex, endIndex);
-	}
-
-	function setTagsFromForm()
-	{
-		var tags = document.getElementById("tagsInput").value.split(",");
-		var startIndex = parseInt(tagsStartIndex.value);
-		var endIndex = parseInt(tagsEndIndex.value);
-		setTags(tags, startIndex, endIndex);
-	}
-
-	function setTitleFromForm()
-	{
-		var startIndex = parseInt(titleStartIndex.value);
-		var endIndex = parseInt(titleEndIndex.value);
-		setTitle(titleInput.value, startIndex, endIndex);
-	}
-
-	function removeFilesFromForm()
-	{
-		var startIndex = parseInt(removeStartIndex.value);
-		var endIndex = parseInt(removeEndIndex.value);
-		flashObj.removeFiles(startIndex, endIndex)
-		console.log(flashObj.getTotalSize());
-	}
-
-	function setGroupId(value)
-	{
-		flashObj.setGroupId(value);
-	}
-
-	function setPermissions(value)
-	{
-		flashObj.setPermissions(value);
-	}
-
-	function setSiteUrl(value)
-	{
-		flashObj.setSiteUrl(value);
-	}
-
-	function setScreenName(value)
-	{
-		flashObj.setScreenName(value);
-	}
-
-	//set parameters to be taken from user input field
-	var tagsInput;
-	var tagsStartIndex;
-	var tagsEndIndex;
-
-	var titleInput;
-	var titleStartIndex;
-	var titleEndIndex;
-
-	var removeStartIndex;
-	var removeEndIndex;
-	var maxUploadsInput;
-
-	var partnerDataInput;
-	var mediaTypeInput
-	var groupId;
-	var permissions;
-	var screenName;
-	var siteUrl;
-
-	function onLoadHandler()
-	{
-		tagsInput = document.getElementById("tagsInput");
-		tagsStartIndex = document.getElementById("tagsStartIndex");
-		tagsEndIndex = document.getElementById("tagsEndIndex");
-
-		titleInput = document.getElementById("titleInput");
-		titleStartIndex = document.getElementById("titleStartIndex");
-		titleEndIndex = document.getElementById("titleEndIndex");
-
-		removeStartIndex = document.getElementById("removeStartIndex");;
-		removeEndIndex = document.getElementById("removeEndIndex");
-
-		maxUploadsInput = document.getElementById("maxUploadsInput");
-		partnerDataInput = document.getElementById("partnerDataInput");
-
-		groupId = document.getElementById("groupId");
-		permissions = document.getElementById("permissions");
-		screenName = document.getElementById("screenName");
-		siteUrl = document.getElementById("siteUrl");
-		mediaTypeInput = document.getElementById("mediaTypeInput");
-	}
 </script>
 
 
 </head>
-<body onload=onLoadHandler()>
+<body onload=onLoadHandler();>
 	<div id="flashContainer">
 		<form>
-			<input type="button" value="Step 1 - Browse & Select">
+			<input type="button" value="Select a File">
 		</form>
 		<div id="uploader"> 
 		</div>
@@ -255,22 +74,29 @@
 			 <!--embed flash object-->
 			swfobject.embedSWF("http://www.kaltura.com/kupload/ui_conf_id/11500", "uploader", "200", "30", "9.0.0", "expressInstall.swf", flashVars, params,attributes);
 		</script>
+    <div id="upload-progress">
+    </div>
 	</div>
 	<br/>
 	<div id="userInput">
-		<form>
+	  <form>
+			<input type="button" value="Cancel" 		        onclick="stopUploads()"><br />
+			<input type="text" value="title here" id="titleInput" /><br />
+			<input type="text" value="tags here, comma separated" id="tagsInput" />
+      <input type="button" value="Save" 	onclick="saveEntry()">
+		</form>
+
+	<!--
+	<form>
 			<input type="button" value="Step 2 - Upload" 		onclick="upload()">
 			<input type="button" value="Step 3 - Add entries" 	onclick="addEntries()">
 			<input type="button" value="Cancel" 		        onclick="stopUploads()">
 		</form>
 
 		<form>
-			<input type="text" id="mediaTypeInput" />
-			<input type="button" value="Set media type" onclick="setMediaType()">
 		</form>
 
 		<form>
-			<input type="button" value="Get Files " onclick="getFiles()">
 		</form>
 
 		<form>
@@ -311,6 +137,7 @@
 			<input id="siteUrl" />
 			<input type="button" value="set site url" onClick="setSiteUrl(siteUrl.value)">
 		</form>
+    -->
 	</div>
 </body>
 </html>

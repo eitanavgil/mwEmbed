3d60700497614ee146e6fadc6e4208be9cf5e5ba
Andrew Davis <andrew.davis@kaltura.com>
2010-09-25 18:19:32 +0000

    * sqlite db added * db migration script created * php interface for ajax calls created
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/kplayer-examples@1006 b58a29cf-3064-46da-94c6-1c29cc75c8e5

---
+++ mediaRSS-Gallery/db.php
@@ -0,0 +1,35 @@
+<?php
+include('db-utils.php');
+$dh = sqlite_open($db, 0666, $err) or die ($err);
+
+if (isset($_POST['save']))
+{
+  if (!empty($_POST['kEntryId']) && !empty($_POST['description']) )
+  {
+    $kEntryId = sqlite_escape_string($_POST['kEntryId']);
+    $description = sqlite_escape_string($_POST['description']);
+    //$name = sqlite_escape_string($_POST['name']);
+    //$email = sqlite_escape_string($_POST['email']);
+    //        if ( empty($_POST['volume']) ) {
+    //                $volume = 'NULL';
+    //                    } else {
+    //                           $volume = "'".sqlite_escape_string($_POST['volume'])."'";
+    //                               }
+    $sql = "INSERT INTO $et (id,kEntryId,description,name,email)
+            VALUES (NULL,'$kEntryId','$description',NULL,NULL);";
+    //     echo "sql = $sql<br />"; // un-comment for debugging
+    $result = sqlite_query($dh, $sql) or die("Error in query: ".sqlite_error_string(sqlite_last_error($dh)));
+    echo "<p><i>Record successfully inserted!</i></p>";
+    }
+  else
+  {
+     echo "<p><i>Incomplete form input. Record not inserted! Go Back</i></p>";
+     }
+  }
+}
+else
+{
+    echo 'Create DB first:<br />';
+}
+  sqlite_close($dh);
+?>

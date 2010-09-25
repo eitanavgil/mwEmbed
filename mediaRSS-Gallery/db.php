a19eee345a4c973e0992b9c662aab83f0cd1b85b
Andrew Davis <andrew.davis@kaltura.com>
2010-09-25 20:45:50 +0000

    seeking help with a bad while loop
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/kplayer-examples@1009 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- mediaRSS-Gallery/db.php
+++ mediaRSS-Gallery/db.php
@@ -1,7 +1,10 @@
 <?php
+// setup db
 trigger_error("db.php is running", E_USER_WARNING);
 include('db-utils.php');
 $dh = sqlite_open($db, 0666, $err) or die ($err);
+
+//if this is a POST, we write a record to the db
 if (isset($_POST['description']))
 {
   if (!empty($_POST['kEntryId']) && !empty($_POST['description']) )
@@ -26,5 +29,36 @@ if (isset($_POST['description']))
      echo "<p><i>Incomplete form input. Record not inserted! Go Back</i></p>";
      }
   }
+
+//if this is a GET, retreive all records and output a javascript object
+if (isset($_GET['pull']))
+{
+  if (!sqlite_is_empty($dh))
+  {
+      $sql = "SELECT * FROM users" ;
+      $result = sqlite_query($dh, $sql);
+
+        echo "descriptions = '[{".
+        while($result->valid()) //{($row=$result->fetch(SQLITE_NUM))
+        {
+          $row=$result->current();
+          echo "'kEntryId':'".$row['kEntryId']."','description':'".$row['description']."'},";
+          $result->next();
+          }
+        echo "]";
+        }
+        
+      else
+      {
+        echo 'Create DB first!<br />';
+        }
+    }
+
+
+?>
+
+
+
+<?php
 sqlite_close($dh);
 ?>

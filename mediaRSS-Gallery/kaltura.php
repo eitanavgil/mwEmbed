04ad04c6ec837afbcf22375a604e24cd5512a498
Andrew Davis <andrew.davis@kaltura.com>
2010-09-26 03:17:48 +0000

    setup showdown
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/kplayer-examples@1019 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- mediaRSS-Gallery/kaltura.php
+++ mediaRSS-Gallery/kaltura.php
@@ -139,7 +139,7 @@ div.thumbnail.selected{
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
 <script type="text/javascript" src="http://html5.kaltura.org/js"></script>  
-<script type="text/javascript" src="libs/showdown.js"></script>
+<script type="text/javascript" src="../libs/showdown.js"></script>
 
 <script type="text/javascript">
 

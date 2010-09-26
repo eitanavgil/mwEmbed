6a77899e8d32756c634ed5478499f398b6498090
Andrew Davis <andrew.davis@kaltura.com>
2010-09-26 19:45:26 +0000

    descriptions are now accessible via javascript
    git-svn-id: http://www.kaltura.org/kalorg/html5video/trunk/kplayer-examples@1024 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- mediaRSS-Gallery/db.php
+++ mediaRSS-Gallery/db.php
@@ -1,5 +1,4 @@
 <?php
-//echo $_SERVER['REQUEST_METHOD'];
 // setup db
 include('db-utils.php');
 $dh = sqlite_open($db, 0666, $err) or die ($err);
@@ -11,16 +10,8 @@ if (isset($_POST['description']))
   {
     $kEntryId = sqlite_escape_string($_POST['kEntryId']);
     $description = sqlite_escape_string($_POST['description']);
-    //$name = sqlite_escape_string($_POST['name']);
-    //$email = sqlite_escape_string($_POST['email']);
-    //        if ( empty($_POST['volume']) ) {
-    //                $volume = 'NULL';
-    //                    } else {
-    //                           $volume = "'".sqlite_escape_string($_POST['volume'])."'";
-    //                               }
     $sql = "INSERT INTO $et (id,kEntryId,description,name,email)
             VALUES (NULL,'$kEntryId','$description',NULL,NULL);";
-    //     echo "sql = $sql<br />"; // un-comment for debugging
     $result = sqlite_query($dh, $sql) or die("Error in query: ".sqlite_error_string(sqlite_last_error($dh)));
     echo "<p><i>Record successfully inserted!</i></p>\n";
     print_r($_POST);
@@ -33,7 +24,7 @@ if (isset($_POST['description']))
 
 //if this is a GET, retreive all records and output a javascript object
 if (isset($_GET))
-echo "descriptions = ";
+//echo "descriptions = ";
 {
   if (!sqlite_is_empty($dh))
   {

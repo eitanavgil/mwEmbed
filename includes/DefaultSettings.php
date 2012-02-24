14cd50191bb8b2d5c1ba5c205f7ab4d52961c3b0
Michael Dale <michael.dale@kaltura.com>
2012-02-24 22:24:21 +0000

    bumped version to 1.6.9a1
    git-svn-id: http://www.kaltura.org/kalorg/html5video/tags/1.6.9/mwEmbed@4200 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -19,7 +19,7 @@ $wgMwEmbedPathUrl = str_replace(
 );
 
 // The version of the library 
-$wgMwEmbedVersion = '1.6.9';
+$wgMwEmbedVersion = '1.6.9a1';
 
 // Url to the resource loader php script: 
 $wgResourceLoaderUrl = $wgMwEmbedPathUrl . 'ResourceLoader.php';
